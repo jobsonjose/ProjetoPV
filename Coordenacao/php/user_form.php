aa7bae8198468aed08ba176c43ae8f30a1333596
ViniciusJDS <vinnye.jacinto@gmail.com>
2017-06-07 13:45:07 -0300

    Tabela de alunos embaixo (transferir pra link externo)

--- Coordenacao/php/user_form.php
+++ Coordenacao/php/user_form.php
@@ -1,78 +1,59 @@
 		<?php
 		require_once 'conexao.php';
-		$ret = $pdo->query("SELECT PRO_NOME, PRO_SIAPE, PRO_EMAIL, SER_NOME, SER_SIAPE, SER_EMAIL, ALN_NOME, ALN_MATRICULA, ALN_EMAIL FROM PS_PROFESSOR, PS_SERVIDOR, PS_ALUNO");
+		$ret = $pdo->query("SELECT * FROM ps_aluno ");
 		$result = $ret->fetchAll();
-		//echo "<pre>";
-		//var_dump($result);
-		//echo "</pre>";
-
 		?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Coordenação do Curso Técnico em Informática para Internet - IFPE -Campus Igarassu</title>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Coordenação do Curso Técnico em Informática para Internet - IFPE -Campus Igarassu</title>
+			<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 
-		<?php include 'estilos.php'; ?>
+			<?php include 'estilos.php'; ?>
 
-	</head>
-	<body>
-		<?php include 'menulateral.php'; ?>
-		<div class="formulario">
-			<h2>Cadastro de Usuário</h2>
-			<form action="user_action.php" method="post" >
-				<label for="nome">Nome</label>
+		</head>
+		<body>
+			<?php include 'menulateral.php'; ?>
+			<div class="formulario">
+				<h2>Cadastro de Usuário</h2>
+				<form action="user_action.php" method="post" >
+					<label for="nome">Nome</label>
 					<input type="text" name="nome"/>
-				<label for="aluno">Matrícula</label>
+					<label for="aluno">Matrícula</label>
 					<input type="text" name="matricula"/>
-				<label for="date">Email:</label><br>
+					<label for="date">Email:</label><br>
 					<input type="email" name="email"/>
-				<select name="tipo" id="selct-tipo">
-					<option value="professor">Professor</option>
-					<option value="servidor">Servidor</option>
-					<option value="aluno">Aluno</option>
-				</select>
+					<select name="tipo" id="selct-tipo">
+						<option value="professor">Professor</option>
+						<option value="servidor">Servidor</option>
+						<option value="aluno">Aluno</option>
+					</select>
+					<input type="submit" value="Enviar" id="btnSalvar" name="submit"/>
+				</form>
+			</div>
 
-
-
-
-				<input type="submit" value="Enviar" id="btnSalvar" name="submit"/>
-			</form>
-		</div>
-
-		<table id="tblListar">
-			<thead>
+			<table id="tblListar">
 				<tr>
 					<th colspan="5">user</th>
 				</tr>
 				<tr>
-					 <th>Nome </th> <th>Matricula</th> <th>Email</th>
+					<th>Nome</th> <th>Matricula</th> <th>Email</th>
+				</tr>
+				<?php
+				foreach($result as $key => $value):
+					?>
+				<tr>
+					<td><?=$value["ALN_NOME"]?></td>
+					<td><?=$value["ALN_MATRICULA"]?></td>
+					<td><?=$value["ALN_EMAIL"]?></td>
+					<td><a href="edit.php?id=$key">Editar</a></td>
+					<td><a href="delete.php?id=$key">Excluir</a></td>
 				</tr>
 
-					<?php
-						for ($i=0; $i < count($result) ; $i++) {
-							echo "<tr>";
-							echo "<td>". $result[$i]["PRO_NOME"]."</td>";
-							echo "<td>". $result[$i]["PRO_SIAPE"]."</td>";
-							echo "<td>". $result[$i]["PRO_EMAIL"]."</td>";
-							echo "</tr>";
-							echo "<tr>";
-							echo "<td>". $result[$i]["SER_NOME"]."</td>";
-							echo "<td>". $result[$i]["SER_SIAPE"]."</td>";
-							echo "<td>". $result[$i]["SER_EMAIL"]."</td>";
-							echo "</tr>";
-							echo "<tr>";
-							echo "<td>". $result[$i]["ALN_NOME"]."</td>";
-							echo "<td>". $result[$i]["ALN_MATRICULA"]."</td>";
-							echo "<td>". $result[$i]["ALN_EMAIL"]."</td>";
-							echo "</tr>";
-
-
-
-						}
-					 ?>
+			<?php endforeach;?>
+		</table>
 
 		<?php include 'footer.php' ?>
 	</body>
-</html>
+	</html>
