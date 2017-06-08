f904cd4abbecbef0ef7ea9d42499dd68ef09452e
LeePablo101 <mleepablo@gmail.com>
2017-06-08 13:40:19 -0300

    desenvolvendo lista

--- Coordenacao/php/form_faltas_professor.php
+++ Coordenacao/php/form_faltas_professor.php
@@ -1,11 +1,3 @@
-	<?php
-		require_once 'conexao.php';
-		$ret = $pdo->query("SELECT FAP_USER_MATRICULA, FAP_DATA, FAP_HORARIO, FAP_DIS FROM PS_FALTAS_PROFESSOR ");
-		$result = $ret->fetchAll();
-		//echo "<pre>";
-		//echo var_dump($result);
-		//echo "</pre>";
-		 ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -32,33 +24,7 @@
 			</form>
 		</div>
 
-		<table id="tblListar">
-			<thead>
-				<tr>
-					<th colspan="5">Registro Falta Professores</th>
-				</tr>
-				<tr>
-					  <th>Disciplina</th> <th>Matrícula do Aluno(a)</th> <th>Data da Aula</th> <th>Horario</th> <th>Matricula</th>
-				</tr>
-			</thead>
-		    <tbody id="corpo">
-				<?php
-					for ($i=0; $i <count($result) ; $i++) {
-						echo "<tr>";
-						echo "<td>". $result[$i]["FAP_DIS"]."</td>";
-						echo "<td>". $result[$i]["FAP_USER_MATRICULA"]."</td>";
-						echo "<td>". $result[$i]["FAP_DATA"]."</td>";
-						echo "<td>". $result[$i]["FAP_HORARIO"]."</td>";
-						echo "<td>". Null."</td>";
-						echo "</tr>";
 
-
-					}
-				?>
-
-
-			</tbody>
-		</table>
 		<?php include 'php/footer.php' ?>
 	</body>
 </html>
