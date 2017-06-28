04d71d64d966a42f4038f8da7a15fb3155ff6625
jobsonjose <jjortb@gmail.com>
2017-06-28 08:58:54 -0300

    delete atualizado

--- Coordenacao/php/delete.php
+++ Coordenacao/php/delete.php
@@ -3,11 +3,11 @@ session_start();
 require_once 'conexao.php';
 
 $id = $_GET['id'];
-var_dump($id);
 
 $del_stmt = $pdo->prepare("DELETE FROM ps_user WHERE USER_ID= ?");
-$del_stmt->execute(array($id));
-header("location:Listagem.php");
+$del_stmt = $pdo->exec("DELETE FROM PS_USER WHERE USER_ID = '$id'");
+
+header("location: Listagem.php");
 
 
 ?>
